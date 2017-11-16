153585ee945085ff44723a61812ab2c8241585eb
chboggs <chboggs@umich.edu>
2017-11-16 16:08:36 -0500

    added ability to add images from hololens

--- backend/flask_app/server.py
+++ backend/flask_app/server.py
@@ -127,6 +127,43 @@ def new_image():
 
     return jsonify({"msg": "Successfully added image"}), Status.HTTP_OK_BASIC
 
+@app.route('/api/new-image-hololens', methods=['POST'])
+def new_image_hololens():
+    username = request.form['username']
+    password = request.form['password']
+
+    if not username or not password:
+        return jsonify({"msg": "Missing login parameter"}), Status.HTTP_BAD_REQUEST
+    if 'image' not in request.files:
+        return jsonify({"msg": "Missing image"}), Status.HTTP_BAD_REQUEST
+    new_im = request.files['image']
+    caption = ""
+    if "caption" in request.form:
+        caption = request.form["caption"]
+    name = new_im.filename
+
+    m = hashlib.new('sha512')
+    m.update(password.encode('utf-8'))
+    password = m.hexdigest()
+    registered_user = User.query.filter(User.username == username, User.password == password).first()
+    if not registered_user:
+        return jsonify({"msg": "Invalid login"}), Status.HTTP_BAD_REQUEST
+
+    if not name or name == '' or len(name) > 50:
+        return jsonify({"msg": "File must have a valid name"}), Status.HTTP_BAD_REQUEST
+
+    path_to_image = os.path.join(image_dir, name)
+    fw = open(path_to_image, 'wb')
+    fw.write(new_im.read())
+    fw.close()
+
+    db.session.add(Image(
+        name=name, owner=username, path_to_image=path_to_image, caption=caption
+        ))
+    db.session.commit()
+
+    return jsonify({"msg": "Successfully added image"}), Status.HTTP_OK_BASIC
+
 @app.route('/api/delete-image', methods=['DELETE'])
 @login_required
 def delete_image():
