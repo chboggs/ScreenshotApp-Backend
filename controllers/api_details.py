8b3e065bbb1c08314ff19d313676d3c4e8c3f433
Alex Wasserman <alexrwasserman@gmail.com>
2017-12-10 15:13:52 -0500

    debugging

--- controllers/api_details.py
+++ controllers/api_details.py
@@ -12,31 +12,43 @@ api_details_blueprint = Blueprint('api_details_blueprint', __name__, template_fo
 
 @api_details_blueprint.route('/api/add_viewer', methods=['POST'])
 def add_viewer_api():
+    print("Got to add viewer")
     image_id = request.form['image_id']
     new_viewer = request.form['new_viewer']
+    print("image_id: " + str(image_id))
+    print("new_viewer: " + str(new_viewer))
 
     if not image_id or not new_viewer:
+        print("first")
         return "Missing required parameter", Status.HTTP_BAD_REQUEST
 
     if not User.query.filter(User.username == new_viewer).first() or not Image.query.filter(Image.id == image_id).first():
+        print("second")
         return "Invalid user or filename", Status.HTTP_BAD_REQUEST
 
+    print("third")
     owner = User.query.filter(User.username == Image.query.filter(Image.id == image_id).first().owner).first()
     current_user = session['username']
     if current_user != owner.username:
+        print("fourth")
         return "You do not own this image", Status.HTTP_BAD_FORBIDDEN
 
     image = Image.query.filter(Image.id == image_id).first()
 
+    print("fifth")
     if new_viewer == owner.username:
+        print("sixth")
         return "User " + new_viewer + " owns this image", Status.HTTP_BAD_REQUEST
     if Viewable.query.filter(Viewable.image_name == image.name and Viewable.user_name == new_viewer).first():
+        print("seventh")
         return "User " + new_viewer + " can already view this image", Status.HTTP_BAD_REQUEST
 
+    print("eighth")
     db.session.add(Viewable(
         image_name=image.name, image_id=image_id, user_name=new_viewer
         ))
     db.session.commit()
+    print("ninth")
 
     return "Successfully granted permission to " + new_viewer, Status.HTTP_OK_BASIC
 
